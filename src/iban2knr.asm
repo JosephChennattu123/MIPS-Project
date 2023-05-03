b93b87b0d2bcc7f35c86293c927ed7120c016937
JosephChennattu123 <joch00001@uni-saarland.de>
2023-05-04 00:52:53 +0200

    second push with modulostr

--- src/iban2knr.asm
+++ src/iban2knr.asm
@@ -7,25 +7,27 @@
 # a1: BLZ buffer (8 bytes)
 # a2: KNR buffer (10 bytes)
 iban2knr:
-addi $a0,$a0,4
 add $t1,$zero,$a1
 add $t2,$zero,$a2
 li $t3 0
 	blz:
-	lb $t4,($a0)
+	lb $t4,4($a0)
 	addi $a0,$a0,1
-	sb $t4,($a1)
-	addi $a1,$a1,1	
+	sb $t4,($t1)
+	addi $t1,$t1,1	
 	addi $t3,$t3,1
 	bne $t3,8,blz
 	
 	knr:
-	lb $t4,($a0)	
+	lb $t4,4($a0)	
 	addi $a0,$a0,1
-	sb $t4,($a2)
-	addi $a2,$a2,1
+	sb $t4,($t2)
+	addi $t2,$t2,1
 	addi $t3,$t3,1
 	bne $t3,18,knr
+
+	move $a1,$t1
+	move $a2,$t2
 	
 # TODO
 	jr	$ra
--- src/moduloStr.asm
+++ src/moduloStr.asm
@@ -10,5 +10,27 @@
 # Return:
 # v0: the decimal number (encoded using ASCII digits '0' to '9') in the buffer [$a0 to $a0 + $a1 - 1] modulo $a2 
 modulo_str:
+	add $t0 $a0 $0
+	add $t1 $a1 $0
+	add $t2 $a2 $0
+	add $t5 $0 $0
+	li $t6 0
+ 	icried:
+ 	beq  $t1 $t6 end
+ 	lb $t3,($t0)
+ 	addi $t0 $t0 1
+ 	subi $t1 $t1 1
+ 	sub $t3 $t3 48
+ 	mul $t5 $t5 10
+ 	add $t5 $t5 $t3
+ 	div $t5,$t2
+ 	mfhi $t5
+ 	b icried
+ 		
+ 		
+ 			
+	
 	# TODO
+	end:
+	move $v0,$t5
 	jr	$ra
