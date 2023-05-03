6b501f6d106042b74da12b1a0595402e722d8062
JosephChennattu123 <joch00001@uni-saarland.de>
2023-05-03 14:03:46 +0200

    -first aufgabe

--- src/iban2knr.asm
+++ src/iban2knr.asm
@@ -7,5 +7,25 @@
 # a1: BLZ buffer (8 bytes)
 # a2: KNR buffer (10 bytes)
 iban2knr:
-	# TODO
+addi $a0,$a0,4
+add $t1,$zero,$a1
+add $t2,$zero,$a2
+li $t3 0
+	blz:
+	lb $t4,($a0)
+	addi $a0,$a0,1
+	sb $t4,($a1)
+	addi $a1,$a1,1	
+	addi $t3,$t3,1
+	bne $t3,8,blz
+	
+	knr:
+	lb $t4,($a0)	
+	addi $a0,$a0,1
+	sb $t4,($a2)
+	addi $a2,$a2,1
+	addi $t3,$t3,1
+	bne $t3,18,knr
+	
+# TODO
 	jr	$ra
