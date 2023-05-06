37448d5913dc053fd6f9e649c18ca58fde81916a
JosephChennattu123 <joch00001@uni-saarland.de>
2023-05-06 11:41:29 +0200

    third aufgabe

--- src/validateChecksum.asm
+++ src/validateChecksum.asm
@@ -7,27 +7,32 @@
 # Arguments:
 # a0 : Address of a string containing a german IBAN (22 characters)
 # Return:
-# v0 : the checksum of the IBAN
+# v0 : the remainder
+
 validate_checksum:
-    # move the first four characters to the end of the IBAN
+    #intial buffer loading
     add $t9 $a0 $zero
     la $t8 temp
     li $t7 18
-    lb $t0, 0($a0)
-    lb $t1, 1($a0)
-    lb $t2, 2($a0)
-    lb $t3, 3($a0)
+    lb $t0, 0($t9)
+    lb $t1, 1($t9)
+    lb $t2, 2($t9)
+    lb $t3, 3($t9)
     addi $t9 $t9 4
+    #loop to extract all digits and place in space temp
     digitloading:
     lb $t4,($t9)
     sb $t4, ($t8)
     addi $t9 $t9 1
     addi $t8 $t8 1
     subi $t7 $t7 1
-    beqz $t7 digitloading
-    li $t4 10   
-    subi $t0 $t0 55
-    div $t0 $t4
+    beqz $t7 digitconversion
+    b digitloading
+    
+    digitconversion:
+    li $t7 10   
+    subi $t0 $t0 55 #converting A to 10,... Z to 35
+    div $t0 $t7
     mflo $t5
     mfhi $t6
     addi $t5 $t5 48
@@ -37,7 +42,7 @@ validate_checksum:
     sb $t6 ($t8)
     addi $t8 $t8 1    
     subi $t1 $t1 55
-    div $t1 $t4
+    div $t1 $t7
     mflo $t5
     mfhi $t6
     addi $t5 $t5 48
@@ -49,10 +54,10 @@ validate_checksum:
     sb $t2 ($t8)
     addi $t8 $t8 1
     sb $t3 ($t8)
-    subi $a0 $t8 23
+    la $a0 temp
     li $a1 24
     li $a2 97
+    move $t4 $ra
     jal modulo_str
-    
-    # restore registers and return
-    jr	$ra
+    move $ra $t4
+    jr $ra                 # return to caller
